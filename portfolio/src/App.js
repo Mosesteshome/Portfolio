55ab7c06ee64c4af455504e978ab43512b6f6cec
Mosesteshome <{new_email}>
2025-05-04 13:07:14 +0300

    deploy fixed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import StarterPage from './components/StarterPage';
 import ServiceDetails from './components/ServiceDetails';
